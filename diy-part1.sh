f0a0efb419452e5262b2cf6a4a3af4f4c40025e2
cdada5454 <2994262185@qq.com>
2025-03-17 23:20:52 +0800

    Create diy-part1.sh

---
+++ diy-part1.sh
@@ -0,0 +1,4 @@
+# Add a feed source
+# echo 'src-git helloworld https://github.com/fw876/helloworld.git' >>feeds.conf.default
+echo 'src-git lucip https://github.com/xiaorouji/openwrt-passwall' >>feeds.conf.default
+echo 'src-git packagep https://github.com/xiaorouji/openwrt-passwall-packages' >>feeds.conf.default
